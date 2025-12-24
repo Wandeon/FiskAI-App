86e745d4a6011172a110177915375cf6c4d20673
Wandeon <mislav@hey.com>
2025-12-24 16:06:42 +0100

    feat(assistant): add HistoryBar component

--- src/components/assistant-v2/index.ts
+++ src/components/assistant-v2/index.ts
@@ -1,3 +1,4 @@
 export { AssistantContainer } from "./AssistantContainer"
 export { AssistantInput } from "./AssistantInput"
 export { SuggestionChips } from "./SuggestionChips"
+export { HistoryBar } from "./HistoryBar"
